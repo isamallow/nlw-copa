ce9b3286847d97bf2ea03de08b41deab02fe3f96
isamallow <isabela.ribeiro1211@gmail.com>
2022-11-04 00:34:33 -0300

    Refatoração, animações e variáveis no CSS

--- js/main.js
+++ js/main.js
@@ -8,9 +8,12 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = -0.4;
+
 function createCard(date, day, games) {
+  delay = delay + 0.4;
   return `
-  <div class="card">
+  <div class="card" style="animation-delay: ${delay}s">
     <h2>${date} <span>${day}</span></h2>
   <ul>
   ${games}
@@ -19,26 +22,19 @@ function createCard(date, day, games) {
   `;
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-  <img src="./assets/logo.svg" alt="Logo da NLW Copa.">
-</header>
-
-<main id="cards">
-  ${createCard('21/11', 'Segunda', 
+document.querySelector("#cards").innerHTML = 
+ createCard('21/11', 'Segunda', 
   createGame('brazil', '8:00', 'cameroon') + 
   createGame('hungary', '13:00', 'argentina') + 
   createGame('colombia', '20:00', 'japan')  
-)}
- ${createCard('24/11', 'Quinta', 
+) +
+ createCard('24/11', 'Quinta', 
   createGame('switzerland', '7:00', 'cameroon') +
   createGame('portugal', '13:00', 'ghana') +
   createGame('brazil', '16:00', 'serbia') 
-)}
-  ${createCard('28/11', 'Segunda', 
+) +
+  createCard('28/11', 'Segunda', 
   createGame('south-korea', '10:00', 'ghana') +
   createGame('brazil', '13:00', 'switzerland') +
   createGame('portugal', '16:00', 'uruguay')  
-)}
-</main>
-`;
+);
